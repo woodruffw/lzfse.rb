d5d2784a5cc979c0fa8f569d6f57ee2752a0c0fe
William Woodruff <william@yossarian.net>
2021-06-01 21:40:22 -0400

    extconf: fix path

--- ext/lzfse/extconf.rb
+++ ext/lzfse/extconf.rb
@@ -7,4 +7,4 @@ RbConfig::MAKEFILE_CONFIG["CC"] = ENV["CC"] if ENV["CC"]
 append_cflags %w[-Os -Wno-unknown-pragmas -Wno-unused-variable -DNDEBUG -D_POSIX_C_SOURCE
                  -std=c99 -fvisibility=hidden -g]
 
-create_makefile "lzfse"
+create_makefile "ext/lzfse"
